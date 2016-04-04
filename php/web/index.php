193e33974a0628db1a0e6a6db7ae76f92771c702
George Moura <gwmoura@gmail.com>
2016-04-04 14:04:31 -0300

    update repo

--- php/web/index.php
+++ php/web/index.php
@@ -1,6 +1,11 @@
+<?php
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+
+$app->get('/', function() use($app) {
+  return "You are in home";
+});
 
 $app->get('/{name}', function($name) use($app) {
     return 'Hello '.$app->escape($name);
--- ruby/hello.rb
+++ ruby/hello.rb
@@ -4,6 +4,9 @@ require "sinatra/reloader" if development?
 set :bind, '0.0.0.0'
 set :port, ENV['PORT']
 
+get '/' do
+  "You are in home"
+end
 get '/:name' do
   "Hello #{params['name']}!"
 end
